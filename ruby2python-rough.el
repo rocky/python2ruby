8db57e169802da405d382f18779752e290ee99f4
rocky <rocky@gnu.org>
2014-09-29 22:48:59 -0400

    Address some bug. Start to handle things in unit testing.

--- ruby2python-rough.el
+++ ruby2python-rough.el
@@ -9,16 +9,19 @@ end's to the python code"
     (dolist (tuple '(
 		     ("elsif \\(.*\\):" "elif \\1") ;; has to come before "if"
 		     ("#!/usr/bin/env ruby" "#!/usr/bin/env python")
-		     ("^\\([:blank:]\\)if \\(.*\\)$" "\\1if \\2:")
-		     ("\\([:blank:]\\)else$" "\\1else:")
-		     ("^\\([:blank:]*\\)unless\\([:blank:]*\\)" "\\1if not\\2")
+		     ("^\\([:blank:]*\\)if \\(.*\\)$" "\\1if \\2:")
+		     ("\\([:blank:]*\\)else$" "\\1else:")
+		     ("^\\([:blank:]*\\)unless\\([:blank:]*.*$\\)" "\\1if not\\2:")
 		     ("String" "basestring")
 		     (".downcase" ".lower")
 		     ("end"       "pass")
 		     (".upcase"   ".upper" )
 		     ("nil"        "None" ) ("true" "True") ("false" "False")
 		     ("begin$"     "try:" )
+
+		     ("require ['\"]test/unit['\"]" import unittest")
 		     ("require '\\(.+\\)'"  "import \\1")
+
 		     ("exit[ 	]*\\(\\(?:[0-9]+\\)\\)$"
 		      "import sys\nsys.exit(\\1)")
 
@@ -30,6 +33,7 @@ end's to the python code"
 		     ;; The following is just for the SolveBio API.
 		     ;; It does no harm otherwise.
 		     ("SolveBio::" "solvebio.")
+		     ("SolveBio." "solvebio.")
 		     ))
       (goto-char (point-min))
       (query-replace-regexp (car tuple) (cdr tuple))
